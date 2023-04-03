f723940666d8cf77e0717e1479f588e18a5d5be5
EmmanuelM7 <mormani11@gmail.com>
2023-04-03 00:18:13 -0400

    first commit

--- Function.cs
+++ Function.cs
@@ -9,7 +9,7 @@ namespace EsepWebhook;
 
 public class Function
 {
-
+    
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
@@ -19,18 +19,18 @@ public class Function
     public string FunctionHandler(object input, ILambdaContext context)
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-
+        
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-
+        
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable(“SLACK_URL”))
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
 
         var response = client.Send(webRequest);
         using var reader = new StreamReader(response.Content.ReadAsStream());
-
+            
         return reader.ReadToEnd();
     }
 }
